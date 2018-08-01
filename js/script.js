f2f6c78d9feadc0603bc596d51ee3f7a82496f59
Alix Cook <acook@appnexus.com>
2018-08-01 11:31:12 -0400

    PR-1 prelim changes to frontend org

--- js/script.js
+++ js/script.js
@@ -1,233 +1,50 @@
-window.onload = function() {
-    screenFadeIn();
-}
-
-
-function goTo(linkString) {
-    screenFadeOut();
-
-
-    setTimeout( function() {
-	window.location.href = linkString;
-    }, 750);
-}
-
-
-function screenFadeIn() {
-    $('body').css('display', 'none').fadeIn(1000);
-}
-
-
-function screenFadeOut() {
-    $('body').fadeOut(500);
-}
-
-
-function toScroll(selectorString) {
-
+var app = angular.module('chiveApp', []);
+app.controller('chiveCtrl', function($scope, $http, $location, $rootScope) {
+  $scope.highSchools = [];
+  $scope.clubs = [];
+  $scope.currentHighSchoolID = null;
+  $scope.scrollTo = function(selectorString) {
     $('html,body').animate({
-        scrollTop: $(selectorString).offset().top
+      scrollTop: $(selectorString).offset().top
     }, 'slow');
 
+    if (selectorString != ".high-school-search") {
+      $("#searchbar-high-school").val('');
+      $("#highSchoolResults").html('');
+    }
+    else if (selectorString != ".club-search") {
+      $("#searchbar-club").val('');
+      $("#clubResults").html('');
+    }
+  }
 
-    setTimeout( function() {
+  $scope.populateHighSchools = function() {
+    console.log('TODO');
+    // TODO move the high school code from the script.js.bak file here
+    $scope.highSchools.push({name: 'my school', address: '123 some street', id: 1});
+    $scope.highSchools.push({name: 'other school', address: '456 other street', id: 2});
+  }
 
-	// cleanup
-
-	if (selectorString != ".high-school-search") {
-	    $("#searchbar-high-school").val('');
-	    $("#highSchoolResults").html('');
-	}
-
-	else if (selectorString != ".club-search") {
-	    $("#searchbar-club").val('');
-	    $("#clubResults").html('');
-	}
-
-
-
-	if (selectorString == ".club-search")
-	    populateClubs();
-    }, 750);
-
-}
-
-
-
-
-function testHighSchool() {
-    // test function
-    $.getJSON('http://localhost:5000/schools', function(data) {
-        var container = document.getElementById('highSchoolResults');
-        html = "";
-        for (var i=0;i<data.length;i++){
-            console.log(data[i]);
-            html += `
-            <div class="card card-block card-outline-primary">
-
-              <p class="card-text" id="0123"> <b>${data[i].name}</b> <br> ${data[i].address} &nbsp;
-                    <span class="btn btn-outline-primary float-right" onclick="highSchoolSelect('${data[i].id}')">Select</span>
-              </p>
-
-            </div>
-            `
-        }
-        container.innerHTML = html;
-    });
-}
-
-
-
-
-
-function populateHighSchools() {
-
-
-    $('.highSchoolResults').fadeOut(500);
-
-    // get string input from input tag
-    // dynamically creates & populates cards in container-results
-
-    testHighSchool();
-
-
-    $('#highSchoolResults').css('display', 'none').fadeIn(500);
-
-}
-
-
-
-
-function highSchoolSelect(highSchool) {
-    // makes sure clubs to be searched in club-search view
-    // is only from this high school
-
-    toScroll('.club-search');
-}
-
-
-
-
-
-
-function testClub() {
-
-    $.getJSON('http://localhost:5000/club?school_id=1', function(data){
-
-
-	var container = document.getElementById('clubResults');
-        html = "";
-        for (var i=0;i<data.length;i++){
-            console.log(data[i]);
-            html += `
-            <div class="card card-block card-outline-primary">
-
-              <p class="card-text" id="0123"> <b>${data[i].name}</b> <br> ${data[i].description} &nbsp;
-                    <span class="btn btn-outline-primary float-right" onclick="highSchoolSelect('0123')">Select</span>
-              </p>
-
-            </div>
-            `
-        }
-        container.innerHTML = html;
-    });
-    
-}
-
-
-
-
-
-
-function populateClubs() {
-
+  $scope.populateClubs = function() {
+    // TODO move the club code from the script.js.bak file here
+    console.log('TODO');
     $('.clubResults').fadeOut(500);
-
-
-    // get string input from input tag
-    // dynamically creates & populates cards in container-results
-
-    if ($("#searchbar-club").val() == "")
-	showSuggestions();
-    else
-	searchClubs($("#searchbar-club").val());
+    var searchVal = $("#searchbar-club").val();
+    // show that we can get the high school id
+    console.log($scope.currentHighSchoolID);
+    $scope.clubs.push({name: 'my cool club', description: 'this is a club', id: 1});
+    $scope.clubs.push({name: 'my lame club', description: 'this is a lame club', id: 2});
 
     $('#clubResults').css('display', 'none').fadeIn(500);
-}
-
-
-
-function searchClubs(searchQuery) {
-    // parses database for matches
-
-    // test
-    showAllClubs();
-}
-
-
-
-
-function showSuggestions() {
-
-
-    var container = document.getElementById('clubResults');
-    html = "";
-
-    html += `
-	<div class="container-suggestion">
-	  <div class="btn btn-outline-primary" onclick="showAllClubs()" id="all-clubs">All clubs</div>
-	  <div class="btn btn-outline-primary" onclick="showNewest()" id="newest-clubs">Newest clubs</div>
-	  <div class="btn btn-outline-primary" onclick="showMostPopular()" id="most-popular">Most popular</div>
-	</div>
-`
-
-    container.innerHTML = html;
-}
-
-
-
-
-
-function showAllClubs() {
-
-    $('.clubResults').fadeOut(500);
-
-
-    // populates all clubs, sorted by alphabetical order
-
-    testClub();
-
-    $('#clubResults').css('display', 'none').fadeIn(500);
-}
-
-
-
-
-
-
-function showNewest() {
-
-    $('.clubResults').fadeOut(500);
-
-    // show newest clubs on top by parsing database
-    // for specified club creation date
-
-    testClub();
-
-    $('#clubResults').css('display', 'none').fadeIn(500);
-}
-
-
-
-
-function showMostPopular() {
-
-    $('.clubResults').fadeOut(500);
-
-    // show most popular clubs on top by parsing database
-    // for specified club population size
-
-    testClub();
-
-    $('#clubResults').css('display', 'none').fadeIn(500);
-}
+  }
+  $scope.goToClub = function(clubId) {
+    console.log('TODO');
+  }
+  $scope.highSchoolSelect = function(schoolId) {
+    console.log(schoolId);
+    console.log('TODO')
+    $scope.scrollTo('.club-search')
+    // TODO load all available clubs here instead
+    $scope.currentHighSchoolID = schoolId;
+  }
+});
