2adec60514e23f68196dbf6725104a337cd62445
unknown <rlin6@stuy.edu>
2018-08-02 19:25:08 -0400

    connected server and database in script

--- js/script.js
+++ js/script.js
@@ -8,21 +8,30 @@ app.controller('chiveCtrl', function($scope, $http, $location, $rootScope) {
       scrollTop: $(selectorString).offset().top
     }, 'slow');
 
-    if (selectorString != ".high-school-search") {
+    /*if (selectorString != ".high-school-search") {
       $("#searchbar-high-school").val('');
       $("#highSchoolResults").html('');
-    }
-    else if (selectorString != ".club-search") {
-      $("#searchbar-club").val('');
-      $("#clubResults").html('');
-    }
-  }
+
+    } 
+    if (selectorString != ".club-search") {
+      //$("#searchbar-club").val('');
+      //$("#clubResults").html('');
+      //$("#clubResults").fadeOut(500);
+    } */
+  } 
 
   $scope.populateHighSchools = function() {
     console.log('TODO');
+    $('.highSchoolResults').fadeOut(500);
+    $.getJSON('http://localhost:5000/schools', function(data) {
+      for (var i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        $scope.highSchools.push({name: data[i].name, address: data[i].address, id: i});
+      }
+    })
+    //$('#highSchoolResults').css('display', 'none').fadeIn(500);
     // TODO move the high school code from the script.js.bak file here
-    $scope.highSchools.push({name: 'my school', address: '123 some street', id: 1});
-    $scope.highSchools.push({name: 'other school', address: '456 other street', id: 2});
+    //$scope.highSchools.push({name: 'other school', address: '456 other street', id: 2});
   }
 
   $scope.populateClubs = function() {
@@ -32,14 +41,23 @@ app.controller('chiveCtrl', function($scope, $http, $location, $rootScope) {
     var searchVal = $("#searchbar-club").val();
     // show that we can get the high school id
     console.log($scope.currentHighSchoolID);
-    $scope.clubs.push({name: 'my cool club', description: 'this is a club', id: 1});
-    $scope.clubs.push({name: 'my lame club', description: 'this is a lame club', id: 2});
+    $.getJSON('http://localhost:5000/club?school_id=1', function(data) {
+      for (var i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        if (data[i].school_id === 1) {
+          $scope.clubs.push({name: data[i].name, description: data[i].description, id: i});
+        }
+      }
+    } )
+    //$scope.clubs.push({name: 'my lame club', description: 'this is a lame club', id: 2});
 
     $('#clubResults').css('display', 'none').fadeIn(500);
   }
+
   $scope.goToClub = function(clubId) {
     console.log('TODO');
   }
+
   $scope.highSchoolSelect = function(schoolId) {
     console.log(schoolId);
     console.log('TODO')
